e7bc6f6c79ee47b7270478fb4282075bea886006
kaminikjha <kaminijhag@gmail.com>
2024-12-24 19:35:30 +0530

    change text size

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -28,7 +28,7 @@ const Partner = () => {
          <h1 className="flex justify-center gap-2 text-2xl md:text-4xl font-bold text-white p-1 md:pb-5">
             Our <span className="text-[#f06321] "> Partnerships</span>
           </h1>
-          <p className="text-sm md:text-3xl  leading-normal  text-white flex justify-center items-center text-center">We collaborate with strategic partners to create value and drive mutual success. By building strong, long-term relationships, we ensure a foundation of trust and innovation. Our partnerships focus on shared goals, enabling us to leverage collective strengths for growth, and sustainable solutions.</p>
+          <p className="text-xs md:text-3xl  leading-normal  text-white flex justify-center items-center text-center">We collaborate with strategic partners to create value and drive mutual success. By building strong, long-term relationships, we ensure a foundation of trust and innovation. Our partnerships focus on shared goals, enabling us to leverage collective strengths for growth, and sustainable solutions.</p>
 
           <div className="flex justify-center p-1 md:pt-10">
           <FaChevronDown onClick={()=> navigate("/partner_fp")} className="flex justify-center rounded-full bg-white text-2xl md:text-5xl p-2 text-[#f06321]"/>
