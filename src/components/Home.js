3b1f9e73b57a270999635870aaa5db8cd55750f6
kaminikjha <kaminijhag@gmail.com>
2025-02-12 19:06:29 +0530

    Animated text added in website

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,42 @@
 import React from 'react';
+import { ReactTyped } from "react-typed";
 
-const HomePage = () => {
+const Home = () => {
   return (
-    <div className="relative h-screen flex items-center justify-center overflow-hidden">
-      {/* Image Background */}
-      <div
-        className="absolute z-0 w-full h-screen bg-cover bg-center"
-        style={{ backgroundImage: `url('https://png.pngtree.com/thumb_back/fh260/background/20240610/pngtree-computer-of-the-software-developer-on-the-desk-in-the-room-image_15746066.jpg')` }}
-      ></div>
+    <div className="relative h-screen flex items-center justify-center text-white overflow-hidden bg-cover">
+      {/* Background Video */}
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute z-0 w-auto min-w-full min-h-full max-w-none"
+      >
+        <source src={"https://cdn.pixabay.com/video/2016/08/22/4733-179738669_large.mp4"} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-      {/* Text Overlay */}
-      <div className="relative z-10 text-center text-white px-4 md:px-40">
-        <div className="group inline-block">
-          <h1 className="text-2xl md:text-5xl font-bold text-white mb-4">
-            <span className="relative">
-              Welcome to Gollamudi Technology and Software
-              <span className="absolute bottom-0 left-0 w-0 h-1 bg-[#f06321] opacity-90 
-                transition-all duration-300 ease-in-out group-hover:w-full"></span>
-            </span>
-          </h1>
-        </div>
+      {/* Content */}
+      <div className="relative z-10 text-center p-5 md:p-20">
+        <h1 className="text-2xl md:text-5xl font-bold text-[#f06321] opacity-90 mb-4"> Welcome to {" "}
+        <ReactTyped
+              strings={[
+                "Gollamudi Technology and Software"
+                
+              ]}
+              typeSpeed={100}
+              loop={true}
+              backSpeed={50}
+            /> </h1>
+        <p className="text-sm md:text-2xl text-white leading-relaxed mb-6">
+        At GTS, we transform ideas into reality. Our expertise lies in building world-class web and mobile applications tailored to meet your business goals. Whether it's designing user-friendly websites or developing powerful software solutions, we deliver innovation that drives success.
+        </p>
+        <p className="text-sm md:text-2xl text-white leading-relaxed mb-6">
+        Are you ready to kickstart your career? Join our team and become part of a dynamic environment that values creativity, growth, and excellence. At GTS, fresh talent is nurtured, and every idea matters.
+        </p>
         
-        <p className="text-sm md:text-2xl text-white leading-relaxed mb-6">
-          At GTS, we transform ideas into reality. Our expertise lies in building world-class web and mobile applications tailored to meet your business goals. Whether it's designing user-friendly websites or developing powerful software solutions, we deliver innovation that drives success.
-        </p>
-        <p className="text-sm md:text-2xl text-white leading-relaxed mb-6">
-          Are you ready to kickstart your career? Join our team and become part of a dynamic environment that values creativity, growth, and excellence. At GTS, fresh talent is nurtured, and every idea matters.
-        </p>
       </div>
     </div>
   );
 };
 
-export default HomePage;
+export default Home;
